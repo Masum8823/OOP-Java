99ec021b8019dd988f9f77aa148df8fd24c83e1b
Masum8823 <masum688823@gmail.com>
2026-04-09 23:48:06 +0600

    Parameterized Constructor added

--- Inheritance/Parameterized_Constructor.java
+++ Inheritance/Parameterized_Constructor.java
@@ -4,3 +4,20 @@
 // ✓ A parameterized constructor to initialize all variables
 // ✓ A method display() to print book details
 // Create 2 Book objects with different values.
+
+class Parameterized_Constructor {
+
+    String title;
+    String author;
+    double price;
+
+    Book(String t, String a, double p) {
+        title = t;
+        author = a;
+        price = p;
+    }
+
+    void display() {
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+
