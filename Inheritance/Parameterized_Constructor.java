739ad65bb615855c7c3c64d9e06bed496e6f94bf
Masum8823 <masum688823@gmail.com>
2026-04-09 23:46:11 +0600

    Paramiterized Constructor Question added

---
+++ Inheritance/Parameterized_Constructor.java
@@ -0,0 +1,6 @@
+// Task 2:
+// Create a class Book with:
+// ✓ title, author, price
+// ✓ A parameterized constructor to initialize all variables
+// ✓ A method display() to print book details
+// Create 2 Book objects with different values.
