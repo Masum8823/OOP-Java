7d84ac2959cdd80d67bf326035165c81b0939702
Masum8823 <masum688823@gmail.com>
2026-04-10 00:16:06 +0600

    Checking all of these codes output

---
+++ Inheritance/Copy_Constructor.java
@@ -0,0 +1,6 @@
+// Task 4:
+// Create a class Person with:
+// ✓ name, age
+// ✓ Constructor with parameters
+// ✓ Another constructor that takes a Person object and copies values
+// Create one object, then copy it into another.
