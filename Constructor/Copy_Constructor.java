5967c87dfd93d2b37cba20db81bd7018f829a288
Masum8823 <masum688823@gmail.com>
2026-04-10 23:46:08 +0600

    Copy Constructor Code Added

--- Constructor/Copy_Constructor.java
+++ Constructor/Copy_Constructor.java
@@ -4,3 +4,34 @@
 // ✓ Constructor with parameters
 // ✓ Another constructor that takes a Person object and copies values
 // Create one object, then copy it into another.
+
+class Person {
+
+    String name;
+    int age;
+
+    Person(String n, int a) {
+        name = n;
+        age = a;
+    }
+    //Copy Constructor
+    Person(Person n) {
+        name = n.name;
+        age = n.age;
+    }
+
+    void display() {
+        System.out.println("Name: " + name + ", Age: " + age);
+    }
+}
+
+public class Copy_Constructor {
+    public static void main(String[] args) {
+
+        Person p1 = new Person("Masum", 23);
+        Person p2 = new Person(p1);
+
+        p1.display();
+        p2.display();
+    }
+}
