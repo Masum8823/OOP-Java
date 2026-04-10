1f2b2d928274ce998d4a536c8bdf0c6439e57fdb
Masum8823 <masum688823@gmail.com>
2026-04-10 23:49:27 +0600

    Simple Bank Account Problem using Constructor

---
+++ Constructor/Simple_BankAccount.java
@@ -0,0 +1,4 @@
+// Create a class BankAccount with:
+// ✓ accountNumber, balance
+// ✓ Constructor that does not allow negative balance (if negative, set to 0)
+// ✓ Method deposit() and withdraw().
