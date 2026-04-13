fab2d19ea039b95f2cead95bf8248da26248aa46
Masum8823 <masum688823@gmail.com>
2026-04-13 22:32:36 +0600

    Task 4 Solution added

--- Inheritance/L2Task4.java
+++ Inheritance/L2Task4.java
@@ -4,3 +4,28 @@
 // ✓ protected accountNumber
 // Create subclass SavingsAccount and test which variables are accessible.
 
+class Account {
+    private double balance = 7000;
+    protected int accountNumber = 12345;
+
+    void showBalance() {
+        System.out.println("Balance: " + balance);
+    }
+}
+
+class SavingsAccount extends Account {
+    void testAccess() {
+
+        // System.out.println(balance); Not accessible (private) we can use setter getter method 
+
+        System.out.println("Account No: " + accountNumber); 
+        showBalance(); 
+    }
+}
+
+public class L2Task4 {
+    public static void main(String[] args) {
+        SavingsAccount s1 = new SavingsAccount();
+        s1.testAccess();
+    }
+}
