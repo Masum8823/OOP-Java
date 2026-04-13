333fe4f1eda9ac34254ea80b205595b00dff4df6
Masum8823 <masum688823@gmail.com>
2026-04-13 22:57:33 +0600

    Single Inheritance Solution added

--- Inheritance/Single_Inheritance.java
+++ Inheritance/Single_Inheritance.java
@@ -8,3 +8,32 @@
 // Assign a value to name.
 // Call both eat() and bark() methods.
 // Display the output.
+
+// Base class (Parent)
+class Animal {
+    String name;
+
+    void eat() {
+        System.out.println(name + " is eating");
+    }
+}
+
+// Derived class (Child)
+class Dog extends Animal {
+
+    void bark() {
+        System.out.println(name + " is barking");
+    }
+}
+
+// Main class
+public class Single_Inheritance {
+    public static void main(String[] args) {
+
+        Dog d1 = new Dog();
+
+        d1.name = "Tommy";   // inherited variable
+        d1.eat();            // inherited method
+        d1.bark();           // own method
+    }
+}
