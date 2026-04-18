d7159ddb80c85405d5ae56ed7292872775a6cf5a
Masum8823 <masum688823@gmail.com>
2026-04-18 15:29:24 +0600

    Interface Question added

---
+++ Inheritance/Interface.java
@@ -0,0 +1,10 @@
+// What is an Interface?
+// Create an interface named Animal.
+// Declare a method eat().
+// Create another interface named Pet.
+// Declare a method play().
+// Create a class Dog that implements both interfaces.
+// Implement both methods.
+// In the main method, create an object of Dog.
+// Call both methods.
+// Display the output.
