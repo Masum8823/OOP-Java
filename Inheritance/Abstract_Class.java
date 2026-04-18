d0a380580eb3d3af61923be4acc8c7c9482ea39c
Masum8823 <masum688823@gmail.com>
2026-04-18 15:24:43 +0600

    Abstract Class Question added

---
+++ Inheritance/Abstract_Class.java
@@ -0,0 +1,11 @@
+// What is an Abstract Class?
+// Create an abstract class named Animal.
+// Declare a variable name.
+// Create a normal method sleep() that prints a message.
+// Create an abstract method eat().
+// Create a class Dog that extends Animal.
+// Implement the eat() method in Dog.
+// In the main method, create an object of Dog.
+// Assign a value to name.
+// Call both sleep() and eat() methods.
+// Display the output.
