4470d11763e3721bc7c705a0d63a451c6d41a46a
Masum8823 <masum688823@gmail.com>
2026-04-18 15:30:13 +0600

    Interface Solution added

--- Inheritance/Interface.java
+++ Inheritance/Interface.java
@@ -8,3 +8,36 @@
 // In the main method, create an object of Dog.
 // Call both methods.
 // Display the output.
+
+// Interface 1
+interface Animal {
+    void eat();
+}
+
+// Interface 2
+interface Pet {
+    void play();
+}
+
+// Class implementing interfaces
+class Dog implements Animal, Pet {
+
+    public void eat() {
+        System.out.println("Dog is eating");
+    }
+
+    public void play() {
+        System.out.println("Dog is playing");
+    }
+}
+
+// Main class
+public class Interface {
+    public static void main(String[] args) {
+
+        Dog d1 = new Dog();
+
+        d1.eat();
+        d1.play();
+    }
+}
