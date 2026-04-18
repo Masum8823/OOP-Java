22c56eca65b9f4c2f097c03221bc3926743b368b
Masum8823 <masum688823@gmail.com>
2026-04-18 15:12:36 +0600

    Hierarchial Inheritance Question added

---
+++ Inheritance/Hierarchical_Inheritance.java
@@ -0,0 +1,11 @@
+// What is Hierarchical Inheritance in Java?
+// Create a base class named Animal.
+// Declare a variable name in the base class.
+// Create a method eat() in the base class that prints a message.
+// Create two derived classes named Dog and Cat that extend Animal.
+// Create a method bark() in the Dog class.
+// Create a method meow() in the Cat class.
+// In the main method, create objects of both Dog and Cat.
+// Assign values to name.
+// Call respective methods.
+// Display the output.
