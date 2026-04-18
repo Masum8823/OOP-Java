3b687dd2da13d280a1dd986313b8a45e711acde0
Masum8823 <masum688823@gmail.com>
2026-04-18 15:18:08 +0600

    Multiple Inheritance Solution added

--- Inheritance/Multiple_Inheritance.java
+++ Inheritance/Multiple_Inheritance.java
@@ -7,3 +7,36 @@
 // In the main method, create an object of Dog.
 // Call both methods.
 // Display the output.
+
+// Interface 1
+interface Animal {
+    void eat();
+}
+
+// Interface 2
+interface Pet {
+    void play();
+}
+
+// Class implementing multiple interfaces
+class Dog implements Animal, Pet {
+
+    public void eat() {
+        System.out.println("Dog is eating");
+    }
+
+    public void play() {
+        System.out.println("Dog is playing");
+    }
+}
+
+// Main class
+public class Multiple_Inheritance {
+    public static void main(String[] args) {
+
+        Dog d1 = new Dog();
+
+        d1.eat();   // from Animal interface
+        d1.play();  // from Pet interface
+    }
+}
