254e1fd9e737c8778757595d4776138ba779893b
Masum8823 <masum688823@gmail.com>
2026-04-18 15:13:49 +0600

    Hierarchial Inheritance Solution added

--- Inheritance/Hierarchical_Inheritance.java
+++ Inheritance/Hierarchical_Inheritance.java
@@ -9,3 +9,44 @@
 // Assign values to name.
 // Call respective methods.
 // Display the output.
+
+// Base class (Parent)
+class Animal {
+    String name;
+
+    void eat() {
+        System.out.println(name + " is eating");
+    }
+}
+
+// Derived class 1 (Child)
+class Dog extends Animal {
+
+    void bark() {
+        System.out.println(name + " is barking");
+    }
+}
+
+// Derived class 2 (Child)
+class Cat extends Animal {
+
+    void meow() {
+        System.out.println(name + " is meowing");
+    }
+}
+
+// Main class
+public class Hierarchical_Inheritance {
+    public static void main(String[] args) {
+
+        Dog d1 = new Dog();
+        d1.name = "Tommy";
+        d1.eat();     // from Animal
+        d1.bark();    // from Dog
+
+        Cat c1 = new Cat();
+        c1.name = "Kitty";
+        c1.eat();     // from Animal
+        c1.meow();    // from Cat
+    }
+}
