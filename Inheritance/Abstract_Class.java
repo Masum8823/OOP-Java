d01ab0f0da64bc78f553b03ff0ccd3950bdf9b65
Masum8823 <masum688823@gmail.com>
2026-04-18 15:25:55 +0600

    Abstract Class Solution added

--- Inheritance/Abstract_Class.java
+++ Inheritance/Abstract_Class.java
@@ -9,3 +9,35 @@
 // Assign a value to name.
 // Call both sleep() and eat() methods.
 // Display the output.
+
+// Abstract class
+abstract class Animal {
+
+    String name;
+
+    void sleep() { // normal method
+        System.out.println(name + " is sleeping");
+    }
+
+    abstract void eat(); // abstract method
+}
+
+// Child class
+class Dog extends Animal {
+
+    void eat() {
+        System.out.println(name + " is eating");
+    }
+}
+
+// Main class
+public class Abstract_Class {
+    public static void main(String[] args) {
+
+        Dog d1 = new Dog();
+
+        d1.name = "Tommy";
+        d1.sleep(); // normal method
+        d1.eat();   // abstract method implemented
+    }
+}
