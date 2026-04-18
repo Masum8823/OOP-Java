8953def258ea0d205806fece581505eab507f79e
Masum8823 <masum688823@gmail.com>
2026-04-18 15:15:30 +0600

    Multiple Inheritance Question Added

---
+++ Inheritance/Multiple_Inheritance.java
@@ -0,0 +1,9 @@
+// What is Multiple Inheritance in Java?
+// Create two interfaces named Animal and Pet.
+// In Animal, create a method eat().
+// In Pet, create a method play().
+// Create a class named Dog that implements both Animal and Pet.
+// Define both eat() and play() methods in Dog.
+// In the main method, create an object of Dog.
+// Call both methods.
+// Display the output.
