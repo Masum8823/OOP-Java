0490b0afeb440d984ce5a5ed832bb20b408796f4
Masum8823 <masum688823@gmail.com>
2026-04-19 21:38:38 +0600

    Method Overriding Question added

---
+++ Polymorphism/Method Overriding/Method_Overriding.java
@@ -0,0 +1,8 @@
+// What is Method Overriding in Java?
+// Create a base class named Animal.
+// Create a method sound() in the base class that prints a message.
+// Create a derived class named Dog that extends Animal.
+// Override the sound() method in the Dog class with a different message.
+// In the main method, create an object of Dog.
+// Call the sound() method.
+// Display the output.
