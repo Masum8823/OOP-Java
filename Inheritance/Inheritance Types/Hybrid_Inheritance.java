fa8cea92747e786b1209efabf0d42b0607c01cf8
Masum8823 <masum688823@gmail.com>
2026-04-19 21:24:13 +0600

    Hybrid Inheritance Solution added

--- Inheritance/Inheritance Types/Hybrid_Inheritance.java
+++ Inheritance/Inheritance Types/Hybrid_Inheritance.java
@@ -11,3 +11,57 @@
 // Assign a value to name.
 // Call all methods.
 // Display the output.
+
+// Base class
+class Animal {
+    String name;
+
+    void eat() {
+        System.out.println(name + " is eating");
+    }
+}
+
+// Derived class
+class Dog extends Animal {
+
+    void bark() {
+        System.out.println(name + " is barking");
+    }
+}
+
+// Interface 1
+interface Pet {
+    void play();
+}
+
+// Interface 2
+interface Friendly {
+    void behave();
+}
+
+// Hybrid class (multilevel + multiple)
+class BabyDog extends Dog implements Pet, Friendly {
+
+    public void play() {
+        System.out.println(name + " is playing");
+    }
+
+    public void behave() {
+        System.out.println(name + " is very friendly");
+    }
+}
+
+// Main class
+public class Hybrid_Inheritance {
+    public static void main(String[] args) {
+
+        BabyDog d1 = new BabyDog();
+
+        d1.name = "Tommy";
+
+        d1.eat();    // from Animal
+        d1.bark();   // from Dog
+        d1.play();   // from Pet
+        d1.behave(); // from Friendly
+    }
+}
