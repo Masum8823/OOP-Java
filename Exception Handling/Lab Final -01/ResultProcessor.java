bbf75dec692ef574655bf275b58bee39ff0ecba4
Masum8823 <masum688823@gmail.com>
2026-05-06 21:11:02 +0600

    Lab Final Question 1

---
+++ Exception Handling/Lab Final -01/ResultProcessor.java
@@ -0,0 +1,7 @@
+// Create a Java program to demonstrate custom exception handling.
+
+// Define a custom exception class InvalidMarkException
+// Create a Student class with:
+// setMarks() method (throws exception if marks not between 0–100)
+// calculateGrade() method to return grade based on marks
+// In the main method, use try-catch to handle invalid marks and display grade or error message
